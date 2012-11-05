dc007b0278996aabfbbfcd079d12e06b1c3b26f8
Colin Jack <colin.jack@gmail.com>
2012-11-05 20:45:40 +0000

    Tests working.

--- spec/testUtil.js
+++ spec/testUtil.js
@@ -2,10 +2,15 @@ module.exports = {
    assertError : function(expectedMessage, done) {
         return function(err) {
             assert.isDefined(err, "Did not expect there to have been an error but got: " + err)
-            // var util = require('util');
-            // util.log(util.inspect(err));
-            assert.match(err, expectedMessage);
-            done();
+            
+            if (expectedMessage instanceof RegExp) {
+                assert.match(err, expectedMessage);
+                done();
+            }
+            else {
+                expectedMessage();
+            }
+            
         }
     },
 
