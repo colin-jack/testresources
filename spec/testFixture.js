c455ba9be2dc86c0ef31b0216a2669ef0e9d7a13
Colin Jack <colin.jack@gmail.com>
2012-11-12 22:02:29 +0000

    Tests are passing but need to make a few fixes.

--- spec/testFixture.js
+++ spec/testFixture.js
@@ -1,4 +1,9 @@
+//require('longjohn')
+
 global.assert = require('chai').assert;
+
+global.winston = require('winston');
+winston.cli();
 
 // Reduce coupling to the directory structur under lib so tests don't break if I move files, and less need for ../../../ style
 // pahts in require
