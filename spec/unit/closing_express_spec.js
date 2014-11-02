eaed57b32e6cbb7ea5f5045659ee00390feb01e0
Colin Jack <colin.jack@gmail.com>
2014-11-02 15:05:28 +0000

    Another interaction test.

--- spec/unit/closing_express_spec.js
+++ spec/unit/closing_express_spec.js
@@ -1,20 +1,26 @@
 ﻿var assert = require('chai').assert;
 var sinon = require('sinon');
-var testresources = require('require-namespace').testresources;
-var closeExpress = testresources.closeExpress;
+var proxyquire = require('proxyquire');
 
 describe("when closing express", function () {
-    var stubExpress;
+    var expressStub;
+    var portManagerStub;
 
     beforeEach(function () {
-        debugger;
-        stubExpress = { close: function () { }, address: function () { } };
-        stubExpress.close = sinon.spy(stubExpress, "close");
-        closeExpress(stubExpress);
+        portManagerStub = { releasePort: function () { } };
+        portManagerStub.releasePort = sinon.spy(portManagerStub, 'releasePort');
+        var underTest = proxyquire('./../../lib/closeExpress', { './portManager': portManagerStub });
+
+        expressStub = { close: function () { }, address: function () { } };
+        expressStub.close = sinon.spy(expressStub, "close");
+        underTest(expressStub);
     });
 
     it("should close provided express instance", function () {
-        debugger;
-        assert.isTrue(stubExpress.close.calledOnce)
+        assert.isTrue(expressStub.close.calledOnce)
+    });
+
+    it("should release the port", function () {
+        assert.isTrue(portManagerStub.releasePort.calledOnce)
     });
 });
