7f98bb79c622408b3a98b95774cf9ac7d129c4ae
Colin Jack <colin.jack@gmail.com>
2014-11-08 14:36:20 +0000

    Tests passing.

--- spec/integration/GET/get_noCache_spec.js
+++ spec/integration/GET/get_noCache_spec.js
@@ -1,34 +1,45 @@
-var testResources = require('require-namespace').testResources;
-var resource = testResources.require('entryPointCreator');
+var resourceTest = require('../../../index');
+var fixture = require('../../testFixture')
+var assert = fixture.assert;
+
 var express = require('express');
+var superAgent = require('superagent');
+
 var testUtil = require('./../testUtil');
+var startServer = fixture.testResources.startServerFluent;
 
-describe("get no cache - ", function() {
-    describe('when you test a get request and resource returns json which is not cacheable', function() {
-        var testBuilder;
-
-        beforeEach(function() {
-            var app = express();
-
-            app.get('/noCache', function(req, res){
-                res.header('Cache-Control', 'no-cache')
-                res.send({ name: 'fido' });
-            });
-
-            testBuilder = resource(app).get('/noCache');
+describe('when you test a get request and resource returns json which is not cacheable', function() {
+    var server;
+    var request;
+    
+    before(function () {
+        var app = express();
+        
+        app.get('/noCache', function (req, res) {
+            res.header('Cache-Control', 'no-cache')
+            res.send({ name: 'fido' });
         });
-
-        it('should pass if your expectations are correct', function(done) {
-            testBuilder
-                .expectNotCached()
-                .run(testUtil.assertNoError(done));
-        });
-
-        it('should fail if you expect caching cached', function(done) {            
-            testBuilder
-               .expectCached("private", 10)
-               .run(testUtil.assertError(/The cache-control value/, done));
-
-        })
+        
+        server = startServer(app);
     })
-});
+    
+    beforeEach(function () {
+        request = superAgent.get('/noCache');
+    });
+    
+    after(function () {
+        server.close();
+    })
+
+    it('should pass if your expectations are correct', function() {
+        return resourceTest(request)
+                            .expectNotCached()
+                            .run(server)
+    });
+    
+    it('should fail if you expect caching cached', function () {
+        return assert.isRejected(resourceTest(request)
+                                                .expectCached("private", 10)
+                                                .run(server), /The cache-control value/);
+    });
+})
