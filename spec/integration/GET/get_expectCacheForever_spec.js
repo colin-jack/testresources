3cef3dfb822f43903dc761714fd0a7ef4b3c1d84
Colin Jack <colin.jack@gmail.com>
2014-11-08 12:53:51 +0000

    Removed dead method.

--- spec/integration/GET/get_expectCacheForever_spec.js
+++ spec/integration/GET/get_expectCacheForever_spec.js
@@ -1,28 +1,31 @@
-var testResources = require('require-namespace').testResources;
-var resource = testResources.require('entryPointCreator');
+var resourceTest = require('../../../index');
+var fixture = require('../../testFixture')
+var assert = fixture.assert;
+
 var express = require('express');
+var superAgent = require('superagent');
+
 var testUtil = require('./../testUtil');
+var startServer = fixture.testResources.startServerFluent;
 
-describe("get cache forever - ", function() {
-    describe('when you test a get request and resource returns json which can be cached for twenty years publically', function() {
-        var testBuilder;
-
-        beforeEach(function() {
-            var app = express();
-
-            app.get('/cacheForever', function(req, res){
-                var twentyYears = 20 * 365 * 24 * 60 * 60;
-                res.header('Cache-Control', 'public, max-age=' + twentyYears)
-                res.send({ name: 'fido' });
-            });
-
-            testBuilder = resource(app).get('/cacheForever');
+describe('when you test a get request and resource returns json which can be cached for twenty years publically', function () {
+    var testBuilder;
+    
+    beforeEach(function () {
+        var app = express();
+        
+        app.get('/cacheForever', function (req, res) {
+            var twentyYears = 20 * 365 * 24 * 60 * 60;
+            res.header('Cache-Control', 'public, max-age=' + twentyYears)
+            res.send({ name: 'fido' });
         });
-
-        it('should pass if your expectation matches', function(done) {
-            testBuilder
-                .expectCachedForever('public')
-                .run(testUtil.assertNoError(done));
-        });
-    })
+        
+        testBuilder = resource(app).get('/cacheForever');
+    });
+    
+    it('should pass if your expectation matches', function (done) {
+        testBuilder
+            .expectCachedForever('public')
+            .run(testUtil.assertNoError(done));
+    });
 });
--- spec/integration/GET/get_spec.js
+++ spec/integration/GET/get_spec.js
@@ -8,86 +8,84 @@ var superAgent = require('superagent');
 var testUtil = require('./../testUtil');
 var startServer = fixture.testResources.startServerFluent;
 
-describe("get - ", function() {
-    describe('when you test a get request', function() {
-        describe('and resource returns cacheable json', function () {
-            var server;
-            var request;
+describe('when you test a get request', function() {
+    describe('and resource returns cacheable json', function () {
+        var server;
+        var request;
             
-            before(function () {
-                var app = express();
+        before(function () {
+            var app = express();
                 
-                app.get('/get', function (req, res) {
-                    res.header('Cache-Control', 'private, max-age=300')
-                    res.send({ name: 'fido' });
-                });
+            app.get('/get', function (req, res) {
+                res.header('Cache-Control', 'private, max-age=300')
+                res.send({ name: 'fido' });
+            });
                 
-                server = startServer(app);
-            })
-            
-            beforeEach(function () {
-                request = superAgent.get('/get')
-            });
-            
-            after(function () {
-                server.close();
-            })
-            
-            it('should pass if your expectations are correct', function () {
-                return resourceTest(request)
-                                .expectBody({ name: 'fido' })
-                                .expectCached("private", 5)
-                                .run(server)
-            });
-            
-            it('should pass if your expectations are correct', function () {
-                return resourceTest(request)
-                                .expectBody({ name: 'fido' })
-                                .expectCached("private", 5)
-                                .run(server)
-            });
-
-            it('should pass if your expectations are correct2', function (done) {
-                resourceTest(request)
-                                .expectBody({ name: 'fido' })
-                                .expectCached("private", 5)
-                                .run(server, done)
-            });
-
-            it('should fail if caching expectation is incorrect', function () {
-                return assert.isRejected(resourceTest(request)
-                                                    .expectCached("private", 10)
-                                                    .run(server));
-            });
-
-            it('should fail if body expectation is incorrect', function () {
-                return assert.isRejected(resourceTest(request)
-                                                    .expectBody({ name: 'spot' })
-                                                    .run(server));
-            }); 
-
-            it('should fail if response code is not expected', function () {
-                return assert.isRejected(resourceTest(request)
-                                                    .expectStatus(400)
-                                                    .run(server), /The status should have been 400./);
-            });
-            
-            it.skip('should fail if you expect it to be cached forever', function () {
-                debugger;
-                var api = resourceTest(request).expectCachedForever();
-                return assert.isRejected(api
-                                                    .run(server), /the body did not match/);
-            });
-            
-            it.skip('should work if body is empty but we expected that', function () {
-            });
-
-            it.skip('should fail if you expect no caching but resource is cached', function () {
-            });
+            server = startServer(app);
         })
-
-        describe('and resource returns something other than JSON', function () {
-            it('should fail');
+            
+        beforeEach(function () {
+            request = superAgent.get('/get')
         });
+            
+        after(function () {
+            server.close();
+        })
+            
+        it('should pass if your expectations are correct', function () {
+            return resourceTest(request)
+                            .expectBody({ name: 'fido' })
+                            .expectCached("private", 5)
+                            .run(server)
+        });
+            
+        it('should pass if your expectations are correct', function () {
+            return resourceTest(request)
+                            .expectBody({ name: 'fido' })
+                            .expectCached("private", 5)
+                            .run(server)
+        });
+
+        it('should pass if your expectations are correct2', function (done) {
+            resourceTest(request)
+                            .expectBody({ name: 'fido' })
+                            .expectCached("private", 5)
+                            .run(server, done)
+        });
+
+        it('should fail if caching expectation is incorrect', function () {
+            return assert.isRejected(resourceTest(request)
+                                                .expectCached("private", 10)
+                                                .run(server));
+        });
+
+        it('should fail if body expectation is incorrect', function () {
+            return assert.isRejected(resourceTest(request)
+                                                .expectBody({ name: 'spot' })
+                                                .run(server));
+        }); 
+
+        it('should fail if response code is not expected', function () {
+            return assert.isRejected(resourceTest(request)
+                                                .expectStatus(400)
+                                                .run(server), /The status should have been 400./);
+        });
+            
+        it.skip('should fail if you expect it to be cached forever', function () {
+            debugger;
+            var api = resourceTest(request).expectCachedForever();
+            return assert.isRejected(api
+                                                .run(server), /the body did not match/);
+        });
+            
+        it.skip('should work if body is empty but we expected that', function () {
+        });
+
+        it.skip('should fail if you expect no caching but resource is cached', function () {
+        });
+    })
+
+    describe('and resource returns something other than JSON', function () {
+        it('should fail');
     });
 });
