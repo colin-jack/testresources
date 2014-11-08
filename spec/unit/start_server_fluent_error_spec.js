9143810daae4fd5e9d159fc7a4180927cc44606a
Colin Jack <colin.jack@gmail.com>
2014-11-08 20:17:22 +0000

    Starting to write proper unit tests.

--- spec/unit/start_server_fluent_error_spec.js
+++ spec/unit/start_server_fluent_error_spec.js
@@ -2,11 +2,32 @@
 var assert = fixture.assert;
 var startServerFluent = fixture.testResources.startServerFluent;
 
-describe("when you ask for a port but the server has not been started", function () {
-    it("should throw an error", function () {
+describe("start server fluent interface", function () {
+    
+    var underTest;
+
+    beforeEach(function () {
         var fakeExpress = {};
-        debugger;
-        var underTest = startServerFluent(fakeExpress);
-        assert.throws(function () { underTest.port }, "The port is not allocated until the server is started.")
+        underTest = startServerFluent(fakeExpress);
     });
-});
+       
+    describe("when you ask for a port but the server has not been started", function () {
+        it("should throw an error", function () {
+            assert.throws(function () { underTest.port }, "The port is not allocated until the server is started.")
+        });
+    });
+
+    describe("when you call run test but don't pass test definition", function () {
+        it("should throw an error", function () {
+            assert.throws(function () { underTest.runTest(null, null) }, "The test definition must be specified.")
+        });
+    });
+
+    describe("when you call run test but pass invalid test definition", function () {
+        it("should throw an error", function () {
+            debugger;
+            assert.throws(function () { underTest.runTest({}, null) }, "The test definition must have a runAgainst method. Please see documentation.")
+        });
+    });
+
+})
